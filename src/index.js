8c462ebbab6c2fb1d3eaaae439f2c64532f04e4a
kjohna <43828763+kjohna@users.noreply.github.com>
2019-01-21 14:06:52 -0700

    set up routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </Router>, document.getElementById('root'));
